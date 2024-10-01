c911248242d7e372d6799faacb28eace25186729
sarahandrew212 <37613860+sarahandrew212@users.noreply.github.com>
2024-10-01 17:21:06 +0500

    script.js

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ document.getElementById("sagittariusIcon").addEventListener("click", function() 
 });
 
 function showSection(section) {
-    const sections = ['earn', 'mine', 'referral', 'wallet'];
     const sectionDiv = document.getElementById("section");
     sectionDiv.style.display = "block";
-    sectionDiv.innerHTML = "";
+    sectionDiv.innerHTML = ""; // Clear previous content
 
     let content = "";
 
     switch (section) {
         case 'earn':
-            content = "You can earn coins by completing tasks!";
+            content = `
+                <h2>Earn Coins</h2>
+                <p>Watch our YouTube channel and earn coins!</p>
+                <a href="https://www.youtube.com/channel/UCXXXXXXXXX" target="_blank">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/YouTube_icon_%282013-2017%29.png" alt="YouTube" width="50" height="50" />
+                    Visit YouTube Channel
+                </a>
+            `;
             break;
         case 'mine':
             content = "Mining coins will take time and effort.";
@@ -28,5 +34,5 @@ function showSection(section) {
             break;
     }
 
-    sectionDiv.innerText = content;
+    sectionDiv.innerHTML = content;
 }
